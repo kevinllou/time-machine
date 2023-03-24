d42e3278dc23eec89f38115a961b5523e2ba8d94
kevinllo <kevingrande2002@gmail.com>
2023-03-24 10:58:15 -0600

    ADD: Routes and its components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import TicTacToe from './pages/TicTacToe/TicTacToe';
+import TimeMachine from './pages/TimeMachine/TimeMachine';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(() => count + 1)} type="button">
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<TimeMachine />} />
+          <Route path="phase2" element={<TicTacToe />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
