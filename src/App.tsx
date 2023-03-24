f4f8539b62f3be8e76e5981364edcf2f924ba486
kevinllo <kevingrande2002@gmail.com>
2023-03-24 11:00:53 -0600

    DELETE: css default files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import TicTacToe from './pages/TicTacToe/TicTacToe';
 import TimeMachine from './pages/TimeMachine/TimeMachine';
 
