67d1d02d0a13bde69759a2c156a06f0e53401dde
kevinllo <kevingrande2002@gmail.com>
2023-03-24 23:40:58 -0600

    ADD: Time Machine styles

--- src/pages/TimeMachine/TimeMachine.tsx
+++ src/pages/TimeMachine/TimeMachine.tsx
@@ -1,10 +1,35 @@
+import Button from '../../components/Button/Button';
 import './TimeMachine.scss';
 
 function TimeMachine() {
   return (
-    <div>
-      <h1>Time Machine</h1>
-    </div>
+    <section className="timeMachine">
+      <div className="timeMachine__container">
+        <div className="timeMachine__grid">
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+          <div className="timeMachine__gridItems" />
+        </div>
+        <div className="timeMachine__options">
+          <Button text="Next" />
+          <Button text="Resume" />
+          <Button text="Previous" />
+        </div>
+      </div>
+    </section>
   );
 }
 
