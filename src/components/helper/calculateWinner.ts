d02deea7c7d4f49cdb9c2f90eade78cb2f71498c
kevinllo <kevingrande2002@gmail.com>
2023-03-27 23:46:36 -0600

    UPDATE: winning patterns in calculatewinner function

--- src/components/helper/calculateWinner.ts
+++ src/components/helper/calculateWinner.ts
@@ -6,6 +6,8 @@ const calculateWinner = (squares: string[]) => {
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < winningPatterns.length; i++) {
